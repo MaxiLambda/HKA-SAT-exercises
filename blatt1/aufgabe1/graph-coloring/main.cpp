b622da59471dca988c45a7bc03efd8de0ddbd43c
maxi <maximilian.lincks@gmx.net>
2024-10-23 18:15:19 +0200

    test random things

--- blatt1/aufgabe1/graph-coloring/main.cpp
+++ blatt1/aufgabe1/graph-coloring/main.cpp
@@ -3,15 +3,55 @@
 #include "third_party/cadical/include/ipasir.h"
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
 
   void* solver = ipasir_init();
+  //FIRST round
 
+  //1 -2 0
   ipasir_add(solver, 1);
   ipasir_add(solver, -2);
   ipasir_add(solver, 0);
 
-  std::cout << ipasir_solve(solver) << std::endl;
+  int result = ipasir_solve(solver);
+
+  if(result == 10) {
+    std::printf("Solvable: \n");
+    for (int i = 1; i <= 2; i++) {
+      std::cout << ipasir_val(solver, i) << " ";
+    }
+    std::cout << std::endl;
+  }
+
+  //SECOND round
+
+  //2 3 0
+  ipasir_add(solver, 2);
+  ipasir_add(solver, 3);
+  ipasir_add(solver, 0);
+
+  //1 0
+  ipasir_add(solver, 1);
+  ipasir_add(solver, 0);
+  int result2 = ipasir_solve(solver);
+
+  if(result2 == 10) {
+    std::printf("Solvable: \n");
+    for (int i = 1; i <= 3; i++) {
+      std::cout << ipasir_val(solver, i) << " ";
+    }
+    std::cout << std::endl;
+  }
+
+  //THIRD round => fails
+  //-1 0 => conflicts with 1 0
+  ipasir_add(solver, -1);
+  ipasir_add(solver, 0);
+
+  int result3 = ipasir_solve(solver);
+  if(result3 == 20) {
+    std::printf("No solvable :(");
+    std::cout << std::endl;
+  }
 
   return 0;
 }
