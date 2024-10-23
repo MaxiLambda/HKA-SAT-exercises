e8735dc7bf93cef52b7418c338ae17a2b8c78be6
maxi <maximilian.lincks@gmx.net>
2024-10-23 18:00:13 +0200

    wire cadical and ipasir up

--- blatt1/aufgabe1/graph-coloring/main.cpp
+++ blatt1/aufgabe1/graph-coloring/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "third_party/cadical/include/cadical.hpp"
+#include "third_party/cadical/include/ipasir.h"
 
 int main() {
   std::cout << "Hello, World!" << std::endl;
+
+  void* solver = ipasir_init();
+
+  ipasir_add(solver, 1);
+  ipasir_add(solver, -2);
+  ipasir_add(solver, 0);
+
+  std::cout << ipasir_solve(solver) << std::endl;
+
   return 0;
 }
